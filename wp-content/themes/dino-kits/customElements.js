28a9bc5c8c1b7a7758ecabf748207f2953e9e55d
LebLeb03 <nole3582@mylaurier.ca>
2024-04-19 16:29:19 -0400

    added products

--- wp-content/themes/dino-kits/customElements.js
+++ wp-content/themes/dino-kits/customElements.js
@@ -9,7 +9,7 @@ customElements.define("x-year", CopyrightYear);
 
 class TwoSidedMarket extends HTMLElement{
 	connectedCallback(){
-this.innerHTML ='<a href="Kit-Submission">Kit Submission Form</a>&nbsp;<a href="Shop-for-Kits">Shop-for-Kits</a>' ;
+this.innerHTML ='<a href="Kit-Submission">Kit Submission Form</a>&nbsp;<a href="Shop-for-Kits">Shop for Kits</a>' ;
 }
 
 
